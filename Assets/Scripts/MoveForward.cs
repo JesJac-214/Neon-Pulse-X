246d63adbf55605ed346aa818ef4720788700787
Tage <tagetjerngren@gmail.com>
2023-02-10 23:57:35 +0100

    Added proper object despawn

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -7,7 +7,7 @@ public class MoveForward : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this, 5);
+        Destroy(gameObject, 5);
     }
 
     // Update is called once per frame
