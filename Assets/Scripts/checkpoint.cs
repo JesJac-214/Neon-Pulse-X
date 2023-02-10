fb9e8aa69109e170afe9df06669924735b3b7627
Tage <tagetjerngren@gmail.com>
2023-02-10 21:42:26 +0100

    Moved reset and added groundwork for lap check

--- Assets/Scripts/checkpoint.cs
+++ Assets/Scripts/checkpoint.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class checkpoint : MonoBehaviour
 {
-    Transform[] childArray;
+    //Transform[] childArray;
     slice[] slices;
+    int totalCheckpoints;
     void Start()
     {
-        childArray = GetComponentsInChildren<Transform>();
+        //childArray = GetComponentsInChildren<Transform>();
         slices = FindObjectsOfType<slice>();
+        totalCheckpoints = slices.Length;
     }
 
     void ResetCheckpoints(int playerID)
     {
-        Debug.Log("Collided with " + playerID);
+        //Debug.Log("Collided with " + playerID);
+        int checkpointsCrossed = 0;
+        foreach (slice slice in slices)
+        {
+            if (slice.collided[playerID])
+            {
+                checkpointsCrossed++;
+            }
+        }
+        
         foreach (slice slice in slices)
         {
             slice.collided[playerID] = false;
