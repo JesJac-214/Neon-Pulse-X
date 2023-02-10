b7a8272a12ba7077c27363799dd8a5c0f659a86d
hhhhhhhh <tagetjerngren@gmail.com>
2023-02-10 17:22:11 +0100

    progress can now be unmade

--- Assets/Scripts/slice.cs
+++ Assets/Scripts/slice.cs
@@ -13,6 +13,10 @@ public class slice : MonoBehaviour
             // !!IMPORTANT!! This may be a bit wack so check back and fix it if it has to be fixed
             other.transform.parent.BroadcastMessage("IncrementProgress");
         }
-        collided[other.transform.parent.GetComponent<vehicle>().playerID] = true;
+        if (collided[other.transform.parent.GetComponent<vehicle>().playerID])
+        {
+            other.transform.parent.BroadcastMessage("DecrementProgress");
+        }
+        collided[other.transform.parent.GetComponent<vehicle>().playerID] = !collided[other.transform.parent.GetComponent<vehicle>().playerID];
     }
 }
--- Assets/Scripts/vehicle.cs
+++ Assets/Scripts/vehicle.cs
@@ -179,4 +179,9 @@ public class vehicle : MonoBehaviour
     {
 		courseProgress++;
     }
+
+	public void DecrementProgress()
+	{
+		courseProgress--;
+	}
 }
