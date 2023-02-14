c57ee174c23dd61e411b85a6a46ab2bdfb5c753b
hhhhhhhh <tagetjerngren@gmail.com>
2023-02-14 10:08:22 +0100

    Obstacle is destroyed upon collision

--- Assets/Scripts/checkpoint.cs
+++ Assets/Scripts/checkpoint.cs
@@ -10,18 +10,22 @@ public class checkpoint : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        Transform player = other.transform.parent;
-        int PlayerID = player.GetComponent<vehicle>().playerID;
+        if (other.gameObject.tag == "Vehicle Body")
+        {
+
+            Transform player = other.transform.parent;
+            int PlayerID = player.GetComponent<vehicle>().playerID;
         
-        if (!collided[PlayerID])
-        {
-            // !!IMPORTANT!! This may be a bit wack so check back and fix it if it has to be fixed
-            player.BroadcastMessage("IncrementProgress");
+            if (!collided[PlayerID])
+            {
+                // !!IMPORTANT!! This may be a bit wack so check back and fix it if it has to be fixed
+                player.BroadcastMessage("IncrementProgress");
+            }
+            if (collided[PlayerID])
+            {
+                player.BroadcastMessage("DecrementProgress");
+            }
+            collided[PlayerID] = !collided[PlayerID];
         }
-        if (collided[PlayerID])
-        {
-            player.BroadcastMessage("DecrementProgress");
-        }
-        collided[PlayerID] = !collided[PlayerID];
     }
 }
--- Assets/Scripts/obstacle.cs
+++ Assets/Scripts/obstacle.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class obstacle : MonoBehaviour
 {
-    void Start()
+    public float destructionDelay = 0.2f;
+    private void OnCollisionEnter(Collision collision)
     {
-        Destroy(gameObject, 20);
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject, destructionDelay);
+        }
     }
 }
