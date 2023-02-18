47581e1e8c8e03bf3af8d2153e4e88d82ead4834
AntonNordgren3 <117897769+AntonNordgren3@users.noreply.github.com>
2023-02-18 16:03:07 +0100

    fungerande boxes

--- Assets/Scripts/Items/ItemBox.cs
+++ Assets/Scripts/Items/ItemBox.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class ItemBox : MonoBehaviour
 {
-    //public GameObject box;
     public GameObject[] ItemEquipmentArray;
+    
     private void OnTriggerEnter(Collider other)
     {
+   
         if (other.gameObject.tag == "Vehicle Body")
         {
             if (other.transform.parent.GetComponent<vehicle>().itemAmmo == 0)
@@ -15,15 +16,17 @@ public class ItemBox : MonoBehaviour
                 other.transform.parent.GetComponent<vehicle>().SecondaryPrefab = ItemEquipmentArray[Random.Range(0, ItemEquipmentArray.Length)];
                 other.transform.parent.GetComponent<vehicle>().itemAmmo = 3;
             }
-            //StartCoroutine("itemDisappear");
-            //box.GetComponent<MeshRenderer>().enabled = false;
+            StartCoroutine("itemDisappear");
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<BoxCollider>().enabled = false;
         }
     }
-    //IEnumerator itemDisappear()
-    //{
-    //    yield return new WaitForSeconds(5);
-    //    box.GetComponent<MeshRenderer>().enabled = true;
-    //}
+    IEnumerator itemDisappear()
+    {
+        yield return new WaitForSeconds(5);
+        GetComponent<MeshRenderer>().enabled = true;
+        GetComponent<BoxCollider>().enabled = true;
+    }
 }
 
 
--- Assets/Scripts/Items/WeaponBox.cs
+++ Assets/Scripts/Items/WeaponBox.cs
@@ -7,10 +7,23 @@ public class WeaponBox : MonoBehaviour
     public GameObject[] weaponEquipmentArray;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Vehicle Body" && other.transform.parent.GetComponent<vehicle>().weaponAmmo == 0)
+        if (other.gameObject.tag == "Vehicle Body")
         {
-            other.transform.parent.GetComponent<vehicle>().PrimaryPrefab = weaponEquipmentArray[Random.Range(0,weaponEquipmentArray.Length)];
-            other.transform.parent.GetComponent<vehicle>().weaponAmmo = 10;
+            if (other.transform.parent.GetComponent<vehicle>().weaponAmmo == 0)
+            {
+                other.transform.parent.GetComponent<vehicle>().PrimaryPrefab = weaponEquipmentArray[Random.Range(0, weaponEquipmentArray.Length)];
+                other.transform.parent.GetComponent<vehicle>().weaponAmmo = 10;
+            }
         }
+        StartCoroutine("itemDisappear");
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+    }
+
+    IEnumerator itemDisappear()
+    {
+        yield return new WaitForSeconds(5);
+        GetComponent<MeshRenderer>().enabled = true;
+        GetComponent<BoxCollider>().enabled = true;
     }
 }
