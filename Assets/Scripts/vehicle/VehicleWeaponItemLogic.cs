851a66908e3fa1f5a511e9b85521a853db22c4e6
Tage <tagetjerngren@gmail.com>
2023-02-24 22:47:13 +0100

    Renamed files for clarity

---
+++ Assets/Scripts/vehicle/VehicleWeaponItemLogic.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class VehicleWeaponItemLogic : MonoBehaviour
+{
+	public GameObject anchor;
+
+	public GameObject WallPrefab;
+	public GameObject CannonBallPrefab;
+
+	public GameObject gun;
+
+	public EquipmentBase Weapon = new EquipmentBase();
+	public EquipmentBase Item = new EquipmentBase();
+
+	public float boostedSpeedValue = 30.0f;
+	public float boostedAccelerationValue = 1000.0f;
+	public float speedCoolDown = 1;
+
+	void Update()
+	{
+		if (Weapon.ammo == 0)
+		{
+			gun.GetComponent<MeshRenderer>().enabled = false;
+		}
+		else
+		{
+			gun.GetComponent<MeshRenderer>().enabled = true;
+		}
+
+	}
+
+		public void OnShoot(InputAction.CallbackContext context)
+	{
+		if (context.ReadValue<float>() == 0)
+		{
+			Weapon.Use(gameObject);
+		}
+	}
+
+	public void OnUseItem(InputAction.CallbackContext context)
+	{
+		if (context.ReadValue<float>() == 0)
+		{
+			Item.Use(gameObject);
+		}
+	}
+
+	public void SpeedBoost()
+	{
+		transform.GetComponent<VehicleDrivingAimingLogic>().maxSpeed += boostedSpeedValue;
+        transform.GetComponent<VehicleDrivingAimingLogic>().accelerationSpeed += boostedAccelerationValue;
+        StartCoroutine("SpeedDuration");
+	}
+	
+	IEnumerator SpeedDuration()
+	{
+		yield return new WaitForSeconds(speedCoolDown);
+		transform.GetComponent<VehicleDrivingAimingLogic>().maxSpeed -= boostedSpeedValue;
+		transform.GetComponent<VehicleDrivingAimingLogic>().accelerationSpeed -= boostedAccelerationValue;
+	}
+	
+	public void SpawnWall()
+	{
+        Instantiate(WallPrefab, transform.position - transform.forward * 2, transform.rotation);
+    }
+
+	public void SpawnCannonBall()
+	{
+        Instantiate(CannonBallPrefab, transform.position + anchor.transform.forward * 5, anchor.transform.rotation);
+    }
+
+	public void Freeze()
+	{
+
+	}
+
+	public void Invert()
+	{
+
+	}
+}
