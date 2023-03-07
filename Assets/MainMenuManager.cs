053fe247a3d96041b55c7a000f6644ca1f2af339
hhhhhhhh <tagetjerngren@gmail.com>
2023-03-07 10:44:29 +0100

    Added the PlayerJoin scene to the main menu

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -10,32 +10,42 @@ public class MainMenuManager : MonoBehaviour
     public GameObject PressAnyButton;
     public GameObject cam;
     public Transform BillboardTransform;
+    public Transform PlayerJoinTransform;
+    private Transform target;
 
-    public GameObject menu;
+    public GameObject MainMenu;
+    public GameObject PlayerJoin;
 
     public AudioSource audioSource;
+    public AudioSource buttonPress;
 
     public float volume = 0.5f;
 
+    private bool atPressAnyButton = true;
     private bool move = false;
 
     void Update()
     {
-        if (!move)
+        if (atPressAnyButton)
         {
             InputSystem.onAnyButtonPress.CallOnce(_ => MoveCameraToMainMenu());
+            atPressAnyButton = false;
         }
         if (move)
         {
-            cam.transform.position = Vector3.Lerp(cam.transform.position, BillboardTransform.position, 4f * Time.deltaTime);
-            cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, BillboardTransform.rotation, 8f * Time.deltaTime);
+            cam.transform.position = Vector3.Lerp(cam.transform.position, target.position, 4f * Time.deltaTime);
+            cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, target.rotation, 8f * Time.deltaTime);
             if ((cam.transform.position - BillboardTransform.position).magnitude < 50f)
             {
-                menu.SetActive(true);
+                MainMenu.SetActive(true);
             }
-            if ((cam.transform.position - BillboardTransform.position).magnitude < 0.1f)
+            else
             {
-                enabled = false;
+                MainMenu.SetActive(false);
+            }
+            if ((cam.transform.position - target.position).magnitude < 0.1f)
+            {
+                move = false;
             }
         }
     }
@@ -44,12 +54,16 @@ public class MainMenuManager : MonoBehaviour
     {
         PressAnyButton.SetActive(false);
         audioSource.Play();
+        target = BillboardTransform;
         move = true;
     }
 
     public void OnPlay()
     {
-        SceneManager.LoadScene("PlayerJoin");
+        target = PlayerJoinTransform;
+        move = true;
+        PlayerJoin.SetActive(true);
+        buttonPress.Play();
     }
 
     public void OnQuit()
