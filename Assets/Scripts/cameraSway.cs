a7d15343a609654fcf95543448ee4cd9c21f7942
hhhhhhhh <tagetjerngren@gmail.com>
2023-03-13 15:27:03 +0100

    Made camera sway frame rate independent

--- Assets/Scripts/cameraSway.cs
+++ Assets/Scripts/cameraSway.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class CameraSway : MonoBehaviour
 {
-    public float amplitude = 0.005f;
+    public float amplitude = 0.5f;
 
     void Update()
     {
-        transform.position += new Vector3(0, Mathf.Cos(Time.time), Mathf.Sin(Time.time)) * amplitude;
+        transform.position += new Vector3(0, Mathf.Cos(Time.time), Mathf.Sin(Time.time)) * amplitude * Time.deltaTime;
     }
 }
