d4b2531032f3fe85254435366f2c13bcc169fa76
hhhhhhhh <tagetjerngren@gmail.com>
2023-03-23 15:29:23 +0100

    Checkpoints no longer count if you don't fully drive through

--- Assets/Scripts/Checkpoints/Checkpoint.cs
+++ Assets/Scripts/Checkpoints/Checkpoint.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Checkpoint : MonoBehaviour
 {
@@ -10,29 +11,54 @@ public class Checkpoint : MonoBehaviour
 
     public bool RespawnPositionTrackerHit = false;
 
-    void OnTriggerEnter(Collider other)
+
+    private float[] PlayerCollisionPositionX = { 0, 0, 0, 0 };
+
+
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Vehicle Body"))
         {
-            if (other.gameObject.transform.parent.transform.parent.gameObject.CompareTag("AIPlayer"))
-            {
-                other.gameObject.transform.parent.GetComponent<AIPlayerLogic>().SteerAI(transform.rotation * Quaternion.Euler(-180, -90, -270));
-            }
+            Transform player = other.transform.parent;
+            int PlayerID = player.GetComponent<VehicleData>().playerID;
+            PlayerCollisionPositionX[PlayerID] = Vector3.Project((other.transform.position - transform.position), transform.up).normalized.x;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        
+        //Debug.Log("Exit:" + Vector3.Project((other.transform.position - transform.position), transform.up).normalized.x);
+        if (other.gameObject.CompareTag("Vehicle Body"))
+        {
             // !!IMPORTANT!! This may be a bit wack so check back and fix it if it has to be fixed
             Transform player = other.transform.parent;
             int PlayerID = player.GetComponent<VehicleData>().playerID;
-
-            player.GetComponent<VehicleData>().lastHitCheckpointTransform = transform;
-
-            if (!collided[PlayerID])
+            Debug.Log(Mathf.Round(PlayerCollisionPositionX[PlayerID] * 100) * 0.01);
+            if (Mathf.Round(Vector3.Project((other.transform.position - transform.position), transform.up).normalized.x * 100) * 0.01 == Mathf.Round(PlayerCollisionPositionX[PlayerID] * 100) * 0.01)
             {
-                player.BroadcastMessage("IncrementProgress");
+                Debug.Log("Did not drive through!\n" + Mathf.Round(Vector3.Project((other.transform.position - transform.position), transform.up).normalized.x * 100) * 0.01 + " == " + Mathf.Round(PlayerCollisionPositionX[PlayerID] * 100) * 0.01);
             }
-            if (collided[PlayerID])
+            else
             {
-                player.BroadcastMessage("DecrementProgress");
+                Debug.Log("Drove through!\n" + Vector3.Project((other.transform.position - transform.position), transform.up).normalized.x + " != " + PlayerCollisionPositionX[PlayerID]);
+                if (other.gameObject.transform.parent.transform.parent.gameObject.CompareTag("AIPlayer"))
+                {
+                    other.gameObject.transform.parent.GetComponent<AIPlayerLogic>().SteerAI(transform.rotation * Quaternion.Euler(-180, -90, -270));
+                }
+
+                player.GetComponent<VehicleData>().lastHitCheckpointTransform = transform;
+
+                if (!collided[PlayerID])
+                {
+                    player.BroadcastMessage("IncrementProgress");
+                }
+                if (collided[PlayerID])
+                {
+                    player.BroadcastMessage("DecrementProgress");
+                }
+                collided[PlayerID] = !collided[PlayerID];
             }
-            collided[PlayerID] = !collided[PlayerID];
         }
         if (other.gameObject.CompareTag("RespawnPositionTrackerCube"))
         {
